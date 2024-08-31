6bd8553a28c4ff4fbac2457b2dae8f7cdbd26c3c
Tyler Kindy <tylerkindy@gmail.com>
2024-08-31 19:01:20 -0400

    Despawn missed fish

--- scenes/fish.gd
+++ scenes/fish.gd
@@ -25,3 +25,7 @@ func _process(delta: float) -> void:
 
 func _on_body_entered(body: Node2D) -> void:
   queue_free()
+
+
+func _on_visible_on_screen_notifier_2d_screen_exited() -> void:
+  queue_free()
