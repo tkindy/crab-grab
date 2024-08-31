e4bfd1f3f3bdc26fd133dd7f05a8a9ff697d9b38
Tyler Kindy <tylerkindy@gmail.com>
2024-08-31 18:05:07 -0400

    Get player and fish together

---
+++ scenes/fish.gd
@@ -0,0 +1,7 @@
+extends Area2D
+
+const SPEED = 120
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+  position.x -= SPEED * delta
