4e575cff7b46fb82928242249146b993767f22e5
Tyler Kindy <tylerkindy@gmail.com>
2024-09-02 16:05:55 -0400

    Delete old fish scenes

--- scenes/fish.gd
+++ scenes/fish.gd
@@ -1,31 +1,5 @@
-extends Area2D
-
-const SPEED = 120
-
-var moving_left: bool
-
-func _ready() -> void:
-  var half_width = $Sprite2D.texture.get_size().x * $Sprite2D.scale.x / 2
-
-  if moving_left:
-    position.x += half_width
-  else:
-    position.x -= half_width
-    scale.x = -1
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-  var x_change = SPEED * delta
-
-  if moving_left:
-    position.x -= x_change
-  else:
-    position.x += x_change
+extends "res://scenes/abstract_fish.gd"
 
 
-func _on_body_entered(body: Node2D) -> void:
-  queue_free()
-
-
-func _on_visible_on_screen_notifier_2d_screen_exited() -> void:
-  queue_free()
+func get_width():
+  return $Sprite2D.texture.get_size().x * $Sprite2D.scale.x
--- scenes/fish_2.gd
+++
@@ -1,5 +0,0 @@
-extends "res://scenes/abstract_fish.gd"
-
-
-func get_width():
-  return $Sprite2D.texture.get_size().x * $Sprite2D.scale.x
