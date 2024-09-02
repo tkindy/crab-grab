94cc88cf547e4044eba1b9b7ee2d226ea80c3f26
Tyler Kindy <tylerkindy@gmail.com>
2024-09-02 17:49:45 -0400

    Don't remove pufferfish on hit

--- scenes/pufferfish.gd
+++ scenes/pufferfish.gd
@@ -3,3 +3,8 @@ extends "res://scenes/abstract_fish.gd"
 
 func get_width() -> int:
   return $Body.texture.get_size().x * $Body.scale.x
+
+
+func _on_body_entered(body: Node2D) -> void:
+  # Override default behavior to remove
+  pass
