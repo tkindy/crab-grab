4bee9652fca3debd76ce1de6a29f8a3967497823
Tyler Kindy <tylerkindy@gmail.com>
2024-09-02 21:05:27 -0400

    Add fish caught animation

--- scenes/fish.gd
+++ scenes/fish.gd
@@ -3,3 +3,12 @@ extends "res://scenes/abstract_fish.gd"
 
 func get_width():
   return $Sprite2D.texture.get_size().x * $Sprite2D.scale.x
+
+
+func _on_body_entered(body: Node2D) -> void:
+  set_deferred("monitoring", false)
+
+  $AnimationPlayer.play("catch")
+  await $AnimationPlayer.animation_finished
+
+  queue_free()
